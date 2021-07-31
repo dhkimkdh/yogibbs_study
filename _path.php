4bdfb1232fd72d84baeacb853b4b7bf5df0c31ff
dhkimkdh <dhkimkdh@naver.com>
2021-07-31 15:42:11 +0900

    기본설정폴더_path.php 루트폴더 /study 로 설정하기

--- _path.php
+++ _path.php
@@ -5,13 +5,13 @@
     define('DB_PASS', 'autoset');
     define('DB_NAME', 'bbs');
     //==== html(src, href..)에서 사용하는 링크폴더 위치 ===
-    define('ROOT_PATH', '/');
-    define('CSS_PATH' , '/css');
-    define('SKIN_PATH', '/skin');
-    define('USER_PATH', '/user');
-    define('MENU_PATH', '/menu');
+    define('ROOT_PATH', '/study');
+    define('CSS_PATH' , '/study/css');
+    define('SKIN_PATH', '/study/skin');
+    define('USER_PATH', '/study/user');
+    define('MENU_PATH', '/study/menu');
     //==== require,include 에 사용하는 절대경로 ========
-    $root = $_SERVER['DOCUMENT_ROOT'] ;
+    $root = $_SERVER['DOCUMENT_ROOT'] .'/study' ;
     define('ROOT_DIR' , $root);
     define('YOGI_DIR' , $root . '/yogi');
     define('SKIN_DIR' , $root . '/skin');
